b30ab0a8ade8d6f4aa1aeef21bcf1c0b865020b6
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-08 03:28:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,13 @@
 class CashRegister
   attr_accessor :total, :discount
 
-  def initalize(total,discount=0)
-    @total = total
-    @discount = discount
-  end
+  # def initalize(total,discount=0)
+  #   @total = total
+  #   @discount = discount
+  # end
+
+  def self.new
+    total = self.new
+    total = 0
 
 end
